0c22b4f0557a42c3701efa0d5f51fcbe21e0a3d8
Mert Akkaya <76126526+mertaky8@users.noreply.github.com>
2024-08-06 14:57:20 +0300

    favUpdated

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
-export { default as HomePage } from "./Homepage";
 export { default as Users, rootLoader as rootLoader } from "./Users";
 export { default as Roots } from "./Roots";
 export { default as UserDetails } from "./UserDetails";
